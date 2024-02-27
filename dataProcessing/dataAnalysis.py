10bac8b68abc71b834d917e79602939992b876ce
Jessica Yang <jessicayangg01@gmail.com>
2024-02-27 16:18:44 -0500

    displaying preliminary/ non changed graphs on ui

--- dataProcessing/dataAnalysis.py
+++ dataProcessing/dataAnalysis.py
@@ -1,29 +1,24 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from interface.graphsView import GraphsView
+# from interface.graphsView import GraphsView
 
 
 class dataAnalysis(object):
     def __init__(self, budgetReader):
         self.budgetReader = budgetReader
 
-    def plot(self):
-        for i in self.budgetReader.getCol():
-            print(i)
+    # def plot(self):
+    #     for i in self.budgetReader.getCol():
+    #         print(i)
 
-        # plt.scatter(
-        #     x = self.budgetReader.dataDict["TV"],
-        #     y = self.budgetReader.dataDict["Sales"],
-        #     )
+    #     # plt.scatter(
+    #     #     x = self.budgetReader.dataDict["TV"],
+    #     #     y = self.budgetReader.dataDict["Sales"],
+    #     #     )
         
 
 
-        # graph = [1,2,3]
-
-        # window = GraphsView()
-        # window.showGraph(graph, 221, "hi")
-        # window.showGraph([2,2,2], 222, "bye")
-        # window.showWindow()
+    #     # window.showWindow()
+            
+        
     
 
     def randomForest(self):
